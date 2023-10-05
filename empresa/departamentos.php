6d69cfa6dd1ba7da84cdc74554f561fd471ce889
Ricardo Pérez <ricpelo@gmail.com>
2023-10-05 20:43:44 +0200

    Con funciones

--- empresa/departamentos.php
+++ empresa/departamentos.php
@@ -7,8 +7,44 @@
 </head>
 <body>
     <?php
+    function mostrar_tabla(PDOStatement $sent)
+    { ?>
+        <table border="1">
+        <thead>
+            <th>Código</th>
+            <th>Denominación</th>
+            <th>Localidad</th>
+        </thead>
+        <tbody>
+            <?php foreach ($sent as $fila): ?>
+                <tr>
+                    <td><?= $fila['codigo'] ?></td>
+                    <td><?= $fila['denominacion'] ?></td>
+                    <td><?= $fila['localidad'] ?></td>
+                </tr>
+            <?php endforeach ?>
+        </tbody>
+    </table>
+    <?php
+    }
+
+    function mostrar_error_departamento_no_existe()
+    { ?>
+        <h3>No se ha encontrado ese departamento.</h3>
+    <?php
+    }
+
+    function cuantos_departamentos($codigo, $pdo)
+    {
+        $sent = $pdo->prepare('SELECT COUNT(*)
+                                 FROM departamentos
+                                WHERE codigo = :codigo');
+        $sent->execute([':codigo' => $codigo]);
+        return $sent->fetchColumn();
+    }
+
     $pdo = new PDO('pgsql:host=localhost;dbname=empresa', 'empresa', 'empresa');
-    $codigo = isset($_GET['codigo']) ? trim($_GET['codigo']) : null;
+    $codigo = isset($_GET['codigo']) ? trim($_GET['codigo']) : '';
     ?>
     <form action="" method="get">
         <label for="codigo">Código:</label>
@@ -16,36 +52,20 @@
         <button type="submit">Buscar</button>
     </form>
     <?php
-    $sent = $pdo->prepare('SELECT COUNT(*)
-                             FROM departamentos
-                            WHERE codigo = :codigo');
-    $sent->execute([':codigo' => $codigo]);
-    $cantidad = $sent->fetchColumn();
-    if ($cantidad == 0): ?>
-        <h3>No se ha encontrado ese departamento.</h3>
-    <?php else:
-        $sent = $pdo->prepare('SELECT *
-                                 FROM departamentos
-                                WHERE codigo = :codigo');
-        $sent->execute([':codigo' => $codigo]);
-        ?>
-        <br>
-        <table border="1">
-            <thead>
-                <th>Código</th>
-                <th>Denominación</th>
-                <th>Localidad</th>
-            </thead>
-            <tbody>
-                <?php foreach ($sent as $fila): ?>
-                    <tr>
-                        <td><?= $fila['codigo'] ?></td>
-                        <td><?= $fila['denominacion'] ?></td>
-                        <td><?= $fila['localidad'] ?></td>
-                    </tr>
-                <?php endforeach ?>
-            </tbody>
-        </table>
-    <?php endif ?>
+    if ($codigo == '') {
+        $sent = $pdo->query('SELECT * FROM departamentos');
+        mostrar_tabla($sent);
+    } else {
+        if (cuantos_departamentos($codigo, $pdo) == 0) {
+            mostrar_error_departamento_no_existe();
+        } else {
+            $sent = $pdo->prepare('SELECT *
+                                     FROM departamentos
+                                    WHERE codigo = :codigo');
+            $sent->execute([':codigo' => $codigo]);
+            mostrar_tabla($sent);
+        }
+    }
+    ?>
 </body>
 </html>
