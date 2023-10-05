2e7ab465696df9c50cefa61bca1690f9d2d41cd6
Ricardo Pérez <ricpelo@gmail.com>
2023-10-05 19:55:44 +0200

    Filtra por departamentos pero no muestra todos

--- empresa/departamentos.php
+++ empresa/departamentos.php
@@ -8,23 +8,44 @@
 <body>
     <?php
     $pdo = new PDO('pgsql:host=localhost;dbname=empresa', 'empresa', 'empresa');
-    $sent = $pdo->query('SELECT * FROM departamentos');
+    $codigo = isset($_GET['codigo']) ? trim($_GET['codigo']) : null;
     ?>
-    <table border="1">
-        <thead>
-            <th>Código</th>
-            <th>Denominación</th>
-            <th>Localidad</th>
-        </thead>
-        <tbody>
-            <?php foreach ($sent as $fila): ?>
-                <tr>
-                    <td><?= $fila['codigo'] ?></td>
-                    <td><?= $fila['denominacion'] ?></td>
-                    <td><?= $fila['localidad'] ?></td>
-                </tr>
-            <?php endforeach ?>
-        </tbody>
-    </table>
+    <form action="" method="get">
+        <label for="codigo">Código:</label>
+        <input type="text" name="codigo" id="codigo" value="<?= $codigo ?>">
+        <button type="submit">Buscar</button>
+    </form>
+    <?php
+    $sent = $pdo->prepare('SELECT COUNT(*)
+                             FROM departamentos
+                            WHERE codigo = :codigo');
+    $sent->execute([':codigo' => $codigo]);
+    $cantidad = $sent->fetchColumn();
+    if ($cantidad == 0): ?>
+        <h3>No se ha encontrado ese departamento.</h3>
+    <?php else:
+        $sent = $pdo->prepare('SELECT *
+                                 FROM departamentos
+                                WHERE codigo = :codigo');
+        $sent->execute([':codigo' => $codigo]);
+        ?>
+        <br>
+        <table border="1">
+            <thead>
+                <th>Código</th>
+                <th>Denominación</th>
+                <th>Localidad</th>
+            </thead>
+            <tbody>
+                <?php foreach ($sent as $fila): ?>
+                    <tr>
+                        <td><?= $fila['codigo'] ?></td>
+                        <td><?= $fila['denominacion'] ?></td>
+                        <td><?= $fila['localidad'] ?></td>
+                    </tr>
+                <?php endforeach ?>
+            </tbody>
+        </table>
+    <?php endif ?>
 </body>
 </html>
