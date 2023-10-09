78a35df8f5e61b5429a422f88cad1bf41d983d2e
Ricardo Pérez <ricpelo@gmail.com>
2023-10-09 21:20:07 +0200

    Creando el comprobar_codigo()

--- empresa/auxiliar.php
+++ empresa/auxiliar.php
@@ -25,3 +25,17 @@ function obtener_post(string $par): ?string
 {
     return isset($_POST[$par]) ? trim($_POST[$par]) : null;
 }
+
+function comprobar_codigo($codigo, &$errores)
+{
+    if ($codigo == '') {
+        $errores[] = 'El código no puede ser vacío';
+        return;
+    }
+    if (mb_strlen($codigo > 2)) {
+        $errores[] = 'El código es demasiado largo';
+    }
+    if (!ctype_digit($codigo)) {
+        $errores[] = 'El código tiene un formato incorrecto';
+    }
+}
