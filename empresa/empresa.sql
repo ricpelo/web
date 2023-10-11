8ad5eca14b18ca3de643a8531f8b84fae119ee8e
Ricardo Pérez <ricpelo@gmail.com>
2023-10-11 17:44:43 +0200

    Crea la tabla departamentos

--- empresa/empresa.sql
+++ empresa/empresa.sql
@@ -1,3 +1,5 @@
+-- Esquema:
+
 DROP TABLE IF EXISTS departamentos CASCADE;
 
 CREATE TABLE departamentos (
@@ -7,8 +9,32 @@ CREATE TABLE departamentos (
     localidad    VARCHAR(255)
 );
 
+DROP TABLE IF EXISTS empleados CASCADE;
+
+CREATE TABLE empleados (
+    id              BIGSERIAL     PRIMARY KEY,
+    numero          NUMERIC(4)    NOT NULL UNIQUE,
+    nombre          VARCHAR(255)  NOT NULL,
+    apellidos       VARCHAR(255),
+    salario         NUMERIC(6,2),
+    fecha_alta      DATE          NOT NULL,
+    departamento_id BIGINT        NOT NULL REFERENCES departamentos (id)
+);
+
+DROP INDEX IF EXISTS idx_empleados_departamento_id CASCADE;
+
+CREATE INDEX idx_empleados_departamento_id ON empleados (departamento_id);
+
+-- Fixtures:
+
 INSERT INTO departamentos (codigo, denominacion, localidad)
     VALUES (10, 'Informática', 'Sanlúcar'),
            (20, 'Administrativo', 'Jerez'),
            (30, 'Prevención', 'Trebujena'),
            (40, 'Laboratorio', 'Chipiona');
+
+INSERT INTO empleados (numero, nombre, apellidos, salario, fecha_alta,
+                       departamento_id)
+    VALUES (1000, 'Pepe', 'García', 1800.25, '2022-04-03'::date, 2),
+           (2000, 'María', 'Rodríguez', 2200.00, '2021-08-05'::date, 1),
+           (3000, 'Rosa', 'González', NULL, '2020-10-14'::date, 3);
